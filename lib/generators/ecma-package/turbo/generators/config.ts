df37f4c4a0d85efc38ec0609e8dd5af97a6f25e5
C. Spencer Beggs <spencer@beg.gs>
2023-11-22 07:38:07 -0500

    Stash

--- lib/generators/ecma-package/turbo/generators/config.ts
+++ lib/generators/ecma-package/turbo/generators/config.ts
@@ -1,6 +1,20 @@
 import type { PlopTypes } from "@turbo/gen";
 
+interface ModifyJSONAction extends PlopTypes.ActionConfig {
+	type: "json-modify-file"; // Point to this action
+	/** Overrides, create non existing */
+	force: boolean;
+	JSONFile: string; // File to modify
+	JSONKey?: string; // Property to append to
+	JSONEntryKey?: string; // Property to add
+	JSONEntryValue: string | object;
+}
+
+type ActionTypes = PlopTypes.ActionType | ModifyJSONAction;
+
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
+	plop.load("plop-pack-json-modify");
+
 	// create a generator
 	plop.setGenerator("ECMA package", {
 		description: "Adds a new ECMA package to the project",
@@ -22,19 +36,29 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 			}
 		],
 		actions: (data) => {
-			console.log(data);
-			const actions: Array<PlopTypes.ActionType> = [];
-			actions.push({
-				type: "add",
-				path: "{{turbo.paths.root}}/{{dir}}/package.json",
-				templateFile: "templates/package.json",
-				transform(template, data) {
-					const pkg = JSON.parse(template);
-					pkg.name = data.pkg.name;
-					pkg.version = data.pkg.version;
-					return JSON.stringify(pkg, null, 2);
-				}
-			});
+			const actions: Array<ActionTypes> = [];
+
+			if (data) {
+				console.log(data);
+				actions.push({
+					type: "add",
+					path: "{{turbo.paths.root}}/{{dir}}/package.json",
+					templateFile: "templates/package.json",
+					transform(template, data) {
+						const pkg = JSON.parse(template);
+						pkg.name = data.pkg.name;
+						pkg.version = data.pkg.version;
+						return JSON.stringify(pkg, null, 2);
+					}
+				});
+				actions.push({
+					type: "json-modify-file",
+					force: false,
+					JSONFile: `${data.turbo.paths.root}/.vscode/settings.json`,
+					JSONKey: "eslint.workingDirectories",
+					JSONEntryValue: `${data.dir}`
+				} as ModifyJSONAction);
+			}
 			return actions;
 		}
 	});
